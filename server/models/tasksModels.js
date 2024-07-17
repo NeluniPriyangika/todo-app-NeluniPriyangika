482eac0e9acfdd07e5d9859c5a7a7fecde8208d0
Neluni Priyangika <96222@fhss.sjp.ac.lk>
2024-07-18 01:30:22 +0530

    created the task model

---
+++ server/models/tasksModels.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const TaskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  priority: {
+    type: String,
+    enum: ['LOW', 'MEDIUM', 'HIGH'],
+    default: 'LOW',
+  },
+  status: {
+    type: String,
+    enum: ['to-do', 'pending', 'completed'],
+    default: 'to-do',
+  },
+  dueDate: {
+    type: Date,
+    required: false,
+  },
+});
+
+module.exports = mongoose.model('Task', TaskSchema);
