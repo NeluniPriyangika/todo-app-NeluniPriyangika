5d22b0d7a19bebb0cbfee166b8bfa96c47701644
Neluni Priyangika <96222@fhss.sjp.ac.lk>
2024-07-18 18:36:23 +0530

    modified the axctivity status

--- client/src/components/ActivityStatus/ActivityStatus.jsx
+++ client/src/components/ActivityStatus/ActivityStatus.jsx
@@ -1,45 +1,16 @@
 import React from 'react';
 import './activityStatus.css';
-import IMG1 from "../../assets/Avatar-1.svg";
-import IMG2 from '../../assets/Avatar-2.svg';
 
-const data = [
-    {
-        id: 1,
-        image: IMG1,
-        title: "Kushantha Charuka created Contract #00124 need John Beige’s signature",
-        date: "Sep 16, 2022 at 11:30 AM",
-    },
-    {
-        id: 2,
-        image: IMG2,
-        title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pretium neque",
-        date: "Sep 16, 2022 at 11:45 AM",
-    },
-    {
-        id: 3,
-        image: IMG2,
-        title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pretium neque",
-        date: "Sep 16, 2022 at 11:45 AM",
-    },
-    // Add more items as needed...
-];
-
-function ActivityStatus() {
+function ActivityStatus({ activities }) {
     return (
         <div className='activitystatus-container'>
             <div className='activitystatus-items'>
-                {data.map(({ id, image, title, date }) => {
-                    return (
-                        <div key={id} className='activityStatus-item'>
-                            <img className='activity-imag' src={image} alt="" />
-                            <div className='activitystatus-cont'>
-                                <div className='activity-status'>{title}</div>
-                                <div className='activity-dat-time'>{date}</div>
-                            </div>
-                        </div>
-                    );
-                })}
+                {activities.map(({ _id, user, action, description, date }) => (
+                    <div key={_id} className='activityStatus-item'>
+                        <div className='activity-status'>{description}</div>
+                        <div className='activity-dat-time'>{new Date(date).toLocaleString()}</div>
+                    </div>
+                ))}
             </div>
         </div>
     );
